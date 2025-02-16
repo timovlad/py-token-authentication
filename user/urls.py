9e9a2be05db129386fef2b92f8708a79ca25eb82
Vlad Tymoshko <slradbez@gmail.com>
2025-02-16 08:52:14 +0100

    solutions test fall

--- user/urls.py
+++ user/urls.py
@@ -5,6 +5,6 @@ from user.views import CreateUserView, LoginUserView, ManageUserView
 app_name = "user"
 
 urlpatterns = [path("register/", CreateUserView.as_view(), name="create"),
-               path("login/", LoginUserView.as_view(), name="get-token"),
-               path("me/", ManageUserView.as_view(), name="manage-user"),
+               path("login/", LoginUserView.as_view(), name="login"),
+               path("me/", ManageUserView.as_view(), name="manage"),
                ]
